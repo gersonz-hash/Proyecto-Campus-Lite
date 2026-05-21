96890bfb489acdc9a8f514dbdb05076d0520fec0
Antony Najarro <anajarrom@miumg.edu.gt>
2026-05-20 18:27:37 -0600

    feat: Pantalla Principal

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,10 +1,13 @@
 package main;
 
+import vista.FrmPrincipal;
+
 public class Main {
 
 	public static void main(String[] args) {
 		
-
+    FrmPrincipal frm = new FrmPrincipal();
+    frm.setVisible(true);
 	}
 
 }
